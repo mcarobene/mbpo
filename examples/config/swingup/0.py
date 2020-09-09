3367e93ee22ff2b4e65c85f6181ab34330d987c2
marcocarobene <33191496+mcarobene@users.noreply.github.com>
2020-09-09 09:19:16 +0100

    Create 0.py

---
+++ examples/config/swingup/0.py
@@ -0,0 +1,35 @@
+params = {
+    'type': 'MBPO',
+    'universe': 'gym',
+    'domain': 'InvertedPendulumSwingupPyBulletEnv',
+    'task': 'v0',
+
+    'log_dir': '~/content/drive/My Drive/Results/Plan1/',
+    'exp_name': 'defaults',
+
+    'kwargs': {
+        'epoch_length': 1000,
+        'train_every_n_steps': 1,
+        'n_train_repeat': 20,
+        'eval_render_mode': None,
+        'eval_n_episodes': 1,
+        'eval_deterministic': True,
+
+        'discount': 0.99,
+        'tau': 5e-3,
+        'reward_scale': 1.0,
+
+        'model_train_freq': 250,
+        'model_retain_epochs': 1,
+        'rollout_batch_size': 100e3,
+        'deterministic': False,
+        'num_networks': 7,
+        'num_elites': 5,
+        'real_ratio': 0.05,
+        'target_entropy': -3,
+        'max_model_t': None,
+        'rollout_schedule': [1, 15, 1, 1],
+        'hidden_dim': 200,
+        'n_initial_exploration_steps': 500,
+    }
+}
